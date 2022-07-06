dc6ec9b497c9ba08c1ed4dd41e73bb246d272b1a
Andy Luu <luuandyf2nd@gmail.com>
2022-07-05 20:30:09 -0700

    Fixed changes

--- server/script.js
+++ server/script.js
@@ -1,11 +1,6 @@
 import http from 'k6/http';
 import { check, sleep, group } from 'k6';
 
-// export let options = {
-// vus: 1000, //stimulate how many virtual users
-// duration: "30s", //how long you want it to run
-// };
-
 export const options = {
   stages: [
     { target: 5000, duration: '30s' },
@@ -16,7 +11,7 @@ export default function test() {
   const reviewTest = http.get(`http://127.0.0.1:3333/reviews?product_id=${Math.floor(Math.random() * (1000000 - 1 + 1)) + 1}`);
   check(reviewTest, { 'status was 200': (r) => r.status === 200 });
   sleep(1);
-  // const metaTest = http.get(`http://localhost:3333/reviews/meta?product_id=${Math.floor(Math.random() * (1000000 - 1 + 1)) + 1}`);
-  // check(metaTest, { 'status was 200': (r) => r.status === 200 });
-  // sleep(1);
+  const metaTest = http.get(`http://localhost:3333/reviews/meta?product_id=${Math.floor(Math.random() * (1000000 - 1 + 1)) + 1}`);
+  check(metaTest, { 'status was 200': (r) => r.status === 200 });
+  sleep(1);
 }
